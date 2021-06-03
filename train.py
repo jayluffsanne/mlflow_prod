044373d75262a47cdb4ccafd08d0edc632da1e61
jayluffsanne <80155008+jayluffsanne@users.noreply.github.com>
2021-06-03 22:14:15 +0530

    Update train.py

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ from sklearn.model_selection import train_test_split
 #from sklearn.linear_model import LinearRegression
 #from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 #import pickle
-import mlflow
+#import mlflow
 #import mlflow.sklearn
 # COMMAND ----------
 
