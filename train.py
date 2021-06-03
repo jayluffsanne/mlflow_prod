f3684eafb81d960ca439cf828a101604d3defe69
jayluffsanne <80155008+jayluffsanne@users.noreply.github.com>
2021-06-03 22:09:19 +0530

    Update train.py

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ import sys
 import pandas as pd
 import numpy as np
 from sklearn.datasets import load_boston
-import seaborn as sn
+# import seaborn as sn
 # import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
